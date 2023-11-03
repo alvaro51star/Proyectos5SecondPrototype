cd55bbe2711092ee245cdaff738e8277151ae71a
alvaro51star <alvaro51star@hotmail.es>
2023-11-03 21:33:13 +0100

    Agua añadida a medias

--- SegundoPrototipoProyectos5/Assets/_Scripts/Player/WaterGun.cs
+++ SegundoPrototipoProyectos5/Assets/_Scripts/Player/WaterGun.cs
@@ -1,16 +1,23 @@
+using System.Globalization;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WaterGun : MonoBehaviour
 {
+    [Header("Water settings")]
     [SerializeField] private float maxWater = 5f;
     [SerializeField] private float thrustForce = 0.5f;
+    [SerializeField] ParticleSystem waterParticleSystem;
     private Rigidbody rb;
 
     private float currentWater;
+    [Header("Player variables")]
     [SerializeField] private GroundCheck groundCheck;
     [SerializeField] private Transform playerCamera;
+
+
+    private bool canMoveWithWater = false;
 
     private void Start()
     {
@@ -21,20 +28,43 @@ public class WaterGun : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetButton("Fire1") && currentWater > 0 && !groundCheck.isGrounded)
+        if (Input.GetButton("Fire1") && currentWater > 0)
         {
             currentWater -= Time.deltaTime;
-            //Activar sistemas de particulas
-            rb.AddForce(-playerCamera.forward * thrustForce, ForceMode.Impulse);
+            waterParticleSystem.Play();
+
+            if (!groundCheck.isGrounded){
+                canMoveWithWater = true;
+            }
+            else{
+                canMoveWithWater = false;
+            }
+        }
+        else
+        {
+            canMoveWithWater = false;
+            waterParticleSystem.Stop();
         }
 
-        if (groundCheck.isGrounded)
+        /*if (groundCheck.isGrounded)
         {
             currentWater = maxWater; //!Esto puede cambiar a que haya otras fuentes de agua.
-        }
+        }*/
 
         Debug.Log(currentWater);
     }
 
+    private void FixedUpdate()
+    {
+        if (canMoveWithWater)
+        {
+            rb.AddForce(-playerCamera.forward * thrustForce, ForceMode.Impulse);
+        }
+    }
 
+    /// <summary>Recarga el agua</summary>
+    public void RechargeWater()
+    {
+        currentWater = maxWater;
+    }
 }
