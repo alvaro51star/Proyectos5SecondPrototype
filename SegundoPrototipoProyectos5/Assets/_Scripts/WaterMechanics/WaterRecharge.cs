08223233b6867a1e630ddd4efe35032e51d76e39
alvaro51star <alvaro51star@hotmail.es>
2023-11-09 17:52:14 +0100

    Arreglo del prefab player
    Ahora el jugador puede elegir cuando pillar el agua.

--- SegundoPrototipoProyectos5/Assets/_Scripts/WaterMechanics/WaterRecharge.cs
+++ SegundoPrototipoProyectos5/Assets/_Scripts/WaterMechanics/WaterRecharge.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,26 +7,22 @@ public class WaterRecharge : MonoBehaviour
 {
     private bool empty = false;
     [SerializeField] private GameObject water;
-    /*private void OnCollisionEnter(Collision other)
-    {
-        if (!empty && !other.gameObject.GetComponent<WaterGun>().IsWaterGunFull())
-        {
-            other.gameObject.GetComponent<WaterGun>().RechargeWater();
-            empty = true;
-            water.SetActive(false);
-        }
-    }*/
 
     private void OnTriggerStay(Collider other)
     {
-        if (!empty && !other.gameObject.GetComponent<WaterGun>().IsWaterGunFull())
+        if (other.gameObject.CompareTag("Player"))
         {
-            if (Input.GetKeyDown(KeyCode.E))
+            if (Input.GetKey(KeyCode.E))
             {
-                other.gameObject.GetComponent<WaterGun>().RechargeWater();
-                empty = true;
-                water.SetActive(false);
+                print("Pulsando E");
+                if (!empty && !other.GetComponent<WaterGun>().IsWaterGunFull())
+                {
+                    other.gameObject.GetComponent<WaterGun>().RechargeWater();
+                    empty = true;
+                    water.SetActive(false);
+                }
             }
+
         }
     }
 }
