6b174fbc3fb79205c8c4e7cc9e9ce878f56c80c6
EHaoqin <espee.delgado@gmail.com>
2023-11-20 22:07:44 +0100

    Mejorando el canvas
    El menú de cambio de escena está en ello, y sigue dando fallos al cambiar de escena TT
    También falta el game over

--- SegundoPrototipoProyectos5/Assets/_Scripts/UI/UI_Manager2.cs
+++ SegundoPrototipoProyectos5/Assets/_Scripts/UI/UI_Manager2.cs
@@ -5,21 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class UI_Manager2 : MonoBehaviour
 {
-    [SerializeField] NextSceneIndex nextSceneIndex;
-
     public UIController m_uiController;
 
     private int initialScene = 0;
     private int levelSelectionScene = 1;
     private int tutorialLevelScene = 2;
-    private int m_nextSceneIndex;
 
     public bool isPaused = false;
+    public bool islevelCompleted = false;
 
     private void Start()
     {
         UIController.instance.EnabledPauseMenu(false);
-        m_nextSceneIndex = nextSceneIndex.nextSceneLoad;
+        UIController.instance.EnabledVictoryMenu(false);
     }
 
     public void QuitGame()
@@ -44,16 +42,37 @@ public class UI_Manager2 : MonoBehaviour
         m_uiController.CursorInvisible();
         SceneManager.LoadScene(tutorialLevelScene);
     }
-
-    /* public void GoToNextLevel()
-    {
-        if (m_nextSceneIndex > PlayerPrefs.GetInt("LevelProgress"))
-        {
-            PlayerPrefs.SetInt("LevelProgress", m_nextSceneIndex);
-        }
-        SceneManager.LoadScene(m_nextSceneIndex);
-    } */
     
+    public void LevelCompleted()    // si con el cambio de escena no se guarda, darle la vuelta o algo.
+    {
+        islevelCompleted = !islevelCompleted;
+        if (islevelCompleted)
+        {
+            Debug.Log("inicio del if");
+            UIController.instance.CursorVisible();
+            Time.timeScale = 0;
+            UIController.instance.EnabledInitialMenu(false);
+            UIController.instance.EnabledLevelsMenu(false);
+            UIController.instance.EnabledPauseMenu(false);
+            UIController.instance.EnabledVictoryMenu(true);
+            UIController.instance.EnabledGameOverMenu(false);
+            Debug.Log("fin del if");
+        }
+        else
+        {
+            Debug.Log("inicio del else");
+            UIController.instance.CursorInvisible();
+            Time.timeScale = 1;
+            UIController.instance.EnabledInitialMenu(false);
+            UIController.instance.EnabledLevelsMenu(false);
+            UIController.instance.EnabledPauseMenu(false);
+            UIController.instance.EnabledVictoryMenu(false);
+            UIController.instance.EnabledGameOverMenu(false);
+            Debug.Log("Fin del else");
+        }
+        Debug.Log("Fin de level completed");
+    }
+
     public void PauseMenu()
     {
         isPaused = !isPaused;
